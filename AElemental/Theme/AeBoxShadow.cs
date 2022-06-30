18c7be64169517054abbbc2d11c13a5bcfb5246b
Yan <yjmrobert@gmail.com>
2022-06-30 11:52:43 -0400

    added AeHeading #25

--- AElemental/Theme/AeBoxShadow.cs
+++ AElemental/Theme/AeBoxShadow.cs
@@ -1,6 +1,16 @@
 namespace AElemental.Theme;
 
-public class AeBoxShadows
+public partial class AeTheme
+{
+    [AeCssVariable] public string AeBoxShadow0 { get; set; } = AeBoxShadows.BoxShadow0;
+    [AeCssVariable] public string AeBoxShadow1 { get; set; } = AeBoxShadows.BoxShadow1;
+    [AeCssVariable] public string AeBoxShadow2 { get; set; } = AeBoxShadows.BoxShadow2;
+    [AeCssVariable] public string AeBoxShadow3 { get; set; } = AeBoxShadows.BoxShadow3;
+    [AeCssVariable] public string AeBoxShadow4 { get; set; } = AeBoxShadows.BoxShadow4;
+    [AeCssVariable] public string AeBoxShadow5 { get; set; } = AeBoxShadows.BoxShadow5;
+}
+
+public static class AeBoxShadows
 {
     public static readonly string BoxShadow0 = "none;";
     public static readonly string BoxShadow1 = "0 1px 3px rgba(0, 0, 0, 0.02), 0 1px 2px rgba(0, 0, 0, 0.14);";
