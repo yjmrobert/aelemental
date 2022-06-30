42f5b37e783b38b5a677670b2aaff9d9cac1abb2
Yan <yrobert@apption.com>
2022-06-30 08:23:16 -0400

    reworked component pages in layout

---
+++ AElemental/Components/AeHeader/AeHeading.razor.cs
@@ -0,0 +1,15 @@
+namespace AElemental.Components;
+
+public partial class AeHeading
+{
+    public struct Variants
+    {
+        // There are 6 variants, h1 through h6
+        public const string H1 = "h1";
+        public const string H2 = "h2";
+        public const string H3 = "h3";
+        public const string H4 = "h4";
+        public const string H5 = "h5";
+        public const string H6 = "h6";
+    }
+}
